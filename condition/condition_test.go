4f3f0d4533e86a436f5ff13ca3d1fb664411eea5
jaronnie <jaron@jaronnie.com>
2024-09-30 10:58:00 +0800

    feat(contrib): condition support or

--- condition/condition_test.go
+++ condition/condition_test.go
@@ -9,24 +9,31 @@ import (
 func TestCondition(t *testing.T) {
 	sqlbuilder.DefaultFlavor = sqlbuilder.PostgreSQL
 
+	var values []any
+	values = append(values, []int{24, 48}, []int{170, 175})
+
 	cds := New(Condition{
-		Field:    "field1",
+		Field:    "name",
 		Operator: Equal,
-		Value:    "value1",
+		Value:    "jaronnie",
 	}, Condition{
-		Field:    "field2",
-		Operator: "=",
-		Value:    "value2",
-	}, Condition{
-		Field:    "field3",
-		Operator: "IN",
-		Value:    nil,
+		Or:          true,
+		OrFields:    []string{"age", "height"},
+		OrOperators: []Operator{Between, Between},
+		OrValues:    values,
 	})
 
-	sb := sqlbuilder.NewSelectBuilder()
+	sb := sqlbuilder.NewSelectBuilder().Select("name", "age", "height").From("user")
 	Apply(sb, cds...)
 
 	sql, args := sb.Build()
 	fmt.Println(sql)
 	fmt.Println(args)
+}
+
+func TestSqlBuilder(t *testing.T) {
+	builder := sqlbuilder.NewSelectBuilder().Select("id", "name").From("user")
+	builder.Where(builder.Or(builder.Equal("id", 1), builder.Equal("id", "2")))
+	builder.Where(builder.And(builder.Equal("name", "jaronnie")))
+	fmt.Println(builder.Build())
 }
