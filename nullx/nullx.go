0a9eddf0db471cae99e899fd0e4524663ba37bc7
jaronnie <jaron@jaronnie.com>
2024-10-28 11:32:26 +0800

    feat(nullx): add NewString

--- nullx/nullx.go
+++ nullx/nullx.go
@@ -8,3 +8,7 @@ import (
 func NewInt(value sql.NullInt64) null.Int {
 	return null.NewInt(value.Int64, value.Valid)
 }
+
+func NewString(value sql.NullString) null.String {
+	return null.NewString(value.String, value.Valid)
+}
