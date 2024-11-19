ab795ffe33eaf583f3fd92048f7b57c777a4e9d6
jaronnie <jaron@jaronnie.com>
2024-11-19 09:36:09 +0800

    feat(condition): do not export AddChain method for Chain

--- condition/chain.go
+++ condition/chain.go
@@ -57,7 +57,7 @@ func NewChainWithConditions(conditions ...Condition) Chain {
 	return Chain{conditions: conditions}
 }
 
-func (c Chain) AddChain(field string, operator Operator, value any, op ...opts.Opt[ChainOperatorOpts]) Chain {
+func (c Chain) addChain(field string, operator Operator, value any, op ...opts.Opt[ChainOperatorOpts]) Chain {
 	o := opts.DefaultApply(op...)
 	c.conditions = append(c.conditions, Condition{
 		Field:        field,
@@ -71,47 +71,47 @@ func (c Chain) AddChain(field string, operator Operator, value any, op ...opts.O
 }
 
 func (c Chain) Equal(field string, value any, op ...opts.Opt[ChainOperatorOpts]) Chain {
-	return c.AddChain(field, Equal, value, op...)
+	return c.addChain(field, Equal, value, op...)
 }
 
 func (c Chain) NotEqual(field string, value any, op ...opts.Opt[ChainOperatorOpts]) Chain {
-	return c.AddChain(field, NotEqual, value, op...)
+	return c.addChain(field, NotEqual, value, op...)
 }
 
 func (c Chain) GreaterThan(field string, value any, op ...opts.Opt[ChainOperatorOpts]) Chain {
-	return c.AddChain(field, GreaterThan, value, op...)
+	return c.addChain(field, GreaterThan, value, op...)
 }
 
 func (c Chain) LessThan(field string, value any, op ...opts.Opt[ChainOperatorOpts]) Chain {
-	return c.AddChain(field, LessThan, value, op...)
+	return c.addChain(field, LessThan, value, op...)
 }
 
 func (c Chain) GreaterEqualThan(field string, value any, op ...opts.Opt[ChainOperatorOpts]) Chain {
-	return c.AddChain(field, GreaterEqualThan, value, op...)
+	return c.addChain(field, GreaterEqualThan, value, op...)
 }
 
 func (c Chain) LessEqualThan(field string, value any, op ...opts.Opt[ChainOperatorOpts]) Chain {
-	return c.AddChain(field, LessEqualThan, value, op...)
+	return c.addChain(field, LessEqualThan, value, op...)
 }
 
 func (c Chain) Like(field string, value any, op ...opts.Opt[ChainOperatorOpts]) Chain {
-	return c.AddChain(field, Like, value, op...)
+	return c.addChain(field, Like, value, op...)
 }
 
 func (c Chain) NotLike(field string, value any, op ...opts.Opt[ChainOperatorOpts]) Chain {
-	return c.AddChain(field, NotLike, value, op...)
+	return c.addChain(field, NotLike, value, op...)
 }
 
 func (c Chain) In(field string, values any, op ...opts.Opt[ChainOperatorOpts]) Chain {
-	return c.AddChain(field, In, values, op...)
+	return c.addChain(field, In, values, op...)
 }
 
 func (c Chain) NotIn(field string, value any, op ...opts.Opt[ChainOperatorOpts]) Chain {
-	return c.AddChain(field, NotIn, value, op...)
+	return c.addChain(field, NotIn, value, op...)
 }
 
 func (c Chain) Between(field string, value any, op ...opts.Opt[ChainOperatorOpts]) Chain {
-	return c.AddChain(field, Between, value, op...)
+	return c.addChain(field, Between, value, op...)
 }
 
 func (c Chain) Or(fields []string, values []any, op ...opts.Opt[ChainOperatorOpts]) Chain {
@@ -128,19 +128,19 @@ func (c Chain) Or(fields []string, values []any, op ...opts.Opt[ChainOperatorOpt
 }
 
 func (c Chain) OrderBy(value any, op ...opts.Opt[ChainOperatorOpts]) Chain {
-	return c.AddChain("", OrderBy, value, op...)
+	return c.addChain("", OrderBy, value, op...)
 }
 
 func (c Chain) Limit(value any, op ...opts.Opt[ChainOperatorOpts]) Chain {
-	return c.AddChain("", Limit, value, op...)
+	return c.addChain("", Limit, value, op...)
 }
 
 func (c Chain) Offset(value any, op ...opts.Opt[ChainOperatorOpts]) Chain {
-	return c.AddChain("", Offset, value, op...)
+	return c.addChain("", Offset, value, op...)
 }
 
 func (c Chain) Page(page, pageSize int, op ...opts.Opt[ChainOperatorOpts]) Chain {
-	return c.AddChain("", Offset, (page-1)*pageSize, op...).AddChain("", Limit, pageSize, op...)
+	return c.addChain("", Offset, (page-1)*pageSize, op...).addChain("", Limit, pageSize, op...)
 }
 
 func (c Chain) ToCondition() []Condition {
