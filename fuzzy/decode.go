b7abe7222a884123881321db94c0ce9efbff1d36
jaron <jaron@jaronnie.com>
2025-04-28 15:43:11 +0800

    fix(fuzzy): sync.Once to register

--- fuzzy/decode.go
+++ fuzzy/decode.go
@@ -1,13 +1,21 @@
 package fuzzy
 
 import (
+	"sync"
+
 	jsoniter "github.com/json-iterator/go"
 	"github.com/json-iterator/go/extra"
 )
 
+var (
+	once sync.Once
+)
+
 func Decode(bodyBytes []byte, req any) ([]byte, error) {
-	RegisterPointerFuzzyDecoders()
-	extra.RegisterFuzzyDecoders() // 启用模糊解码
+	once.Do(func() {
+		RegisterPointerFuzzyDecoders()
+		extra.RegisterFuzzyDecoders() // 启用模糊解码
+	})
 
 	if err := jsoniter.Unmarshal(bodyBytes, &req); err != nil {
 		return nil, err
